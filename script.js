b8c911f3c93d132b95660f52765d6070c336070d
Clint G <cgardner4@gmail.com>
2022-10-04 18:15:04 +0200

    finalise basic gui and fix winning logic

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ let playerScore = 0;
 let playerChoice = '';
 let computerChoice = '';
 let outcome = '';
+let finalOutcome = '';
 
 
 
@@ -36,43 +37,46 @@ let outcome = '';
 btnRock.addEventListener('click', function () {
     computerChoice = getComputerChoice();
     playerChoice = 'rock';
-    playerChoice.toLowerCase();
+   
     console.log(playRound(playerChoice, computerChoice));
     console.log(playerChoice, playerScore);
     console.log(computerChoice, computerScore);
     document.getElementById("pScore").textContent = playerScore;
     document.getElementById("cScore").textContent = computerScore;
     document.getElementById("showOutcome").textContent = outcome;
+    document.getElementById("finalOutcome").textContent = finalOutcome;
 });
 
 btnScissors.addEventListener('click', function () {
     computerChoice = getComputerChoice();
     playerChoice = 'scissors';
-    playerChoice.toLowerCase();
+    
     console.log(playRound(playerChoice, computerChoice));
     console.log(playerChoice, playerScore);
     console.log(computerChoice, computerScore);
     document.getElementById("pScore").textContent = playerScore;
     document.getElementById("cScore").textContent = computerScore;
     document.getElementById("showOutcome").textContent = outcome;
+    document.getElementById("finalOutcome").textContent = finalOutcome;
 });
 
 btnPaper.addEventListener('click', function () {
     computerChoice = getComputerChoice();
     playerChoice = 'paper';
-    playerChoice.toLowerCase();
+    console.log(playRound(playerChoice, computerChoice));
     console.log(playerChoice, playerScore);
     console.log(computerChoice, computerScore);
     document.getElementById("pScore").textContent = playerScore;
     document.getElementById("cScore").textContent = computerScore;
     document.getElementById("showOutcome").textContent = outcome;
+    document.getElementById("finalOutcome").textContent = finalOutcome;
 });
 
 
 
 
 
-playerChoice.toLowerCase();
+
 console.log(playerChoice);
 
 
@@ -95,44 +99,17 @@ console.log(playerChoice);
 function playRound(playerChoice, ComputerChoice) {
     if (playerChoice === 'rock' && computerChoice === 'Scissors') {
         playerScore++;
-        if (playerScore > 4) {
-            outcome = "That's 5 points, You've Won the Game!"
-        } else if (computerScore > 4) {
-            outcome = "That's 5 points for the Computer, You Lose!"
-        } else {
-            outcome = "You win! Rock beats Scissors.";
-        }
+        outcome = "You win! Rock beats Scissors.";
     } else if (playerChoice === 'rock' && computerChoice === 'Rock') {
         outcome = "It's a tie! You both picked Rock.";
     } else if (playerChoice === 'rock' && computerChoice === 'Paper') {
         computerScore++;
-        if (playerScore > 4) {
-            outcome = "That's 5 points, You've Won the Game!"
-        } else if (computerScore > 4) {
-            outcome = "That's 5 points for the Computer, You Lose!"
-        } else {
-            outcome = "You win! Rock beats Scissors.";
-        }
         outcome = "You lose! Paper beats Rock.";
     } else if (playerChoice === 'paper' && computerChoice === 'Scissors') {
         computerScore++;
-        if (playerScore > 4) {
-            outcome = "That's 5 points, You've Won the Game!"
-        } else if (computerScore > 4) {
-            outcome = "That's 5 points for the Computer, You Lose!"
-        } else {
-            outcome = "You win! Rock beats Scissors.";
-        }
-        outcome = "You lose! Scissors beats Paper."
+        outcome = "You lose! Scissors beats Paper.";
     } else if (playerChoice === 'paper' && computerChoice === 'Rock') {
         playerScore++;
-        if (playerScore > 4) {
-            outcome = "That's 5 points, You've Won the Game!"
-        } else if (computerScore > 4) {
-            outcome = "That's 5 points for the Computer, You Lose!"
-        } else {
-            outcome = "You win! Rock beats Scissors.";
-        }
         outcome = "You win! Paper beats Rock.";
     } else if (playerChoice === 'paper' && computerChoice === 'Paper') {
         outcome = "It's a tie! you both picked Paper.";
@@ -140,24 +117,16 @@ function playRound(playerChoice, ComputerChoice) {
         outcome = "It's a tie! You both picked Scissors.";
     } else if (playerChoice === 'scissors' && computerChoice === 'Rock') {
         computerScore++;
-        if (playerScore > 4) {
-            outcome = "That's 5 points, You've Won the Game!"
-        } else if (computerScore > 4) {
-            outcome = "That's 5 points for the Computer, You Lose!"
-        } else {
-            outcome = "You win! Rock beats Scissors.";
-        }
-        outcome = "You lose! Rock beats Scissors."
+        outcome = "You lose! Rock beats Scissors.";
     } else {
         playerScore++;
-        if (playerScore > 4) {
-            outcome = "That's 5 points, You've Won the Game!"
-        } else if (computerScore > 4) {
-            outcome = "That's 5 points for the Computer, You Lose!"
-        } else {
-            outcome = "You win! Rock beats Scissors.";
-        }
         outcome = "You win! Scissors beats Paper.";
+    }
+
+   if (playerScore == 5) {
+        finalOutcome = "That's Five Points, You've Won the Game!"
+    } else if (computerScore == 5) {
+        finalOutcome = "That's Five Points for the Computer, You Lose!"
     }
 }
 
